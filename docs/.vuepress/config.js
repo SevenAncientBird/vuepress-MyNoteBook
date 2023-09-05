705e238ed2910bb154ae3c406812f7fbea0cc779
SevenAncientBird <836932818@qq.com>
2023-09-05 16:44:36 +0800

    2023/9/5

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title:'学习笔记',
+    description:'Just Playing around',
+    base:'/',
+    dest:'./ROOT',
+    head:[],
+    plugins:[],
+    themeConfig:{
+        nav:[
+            { text:'主页',link:'/README.md' }
+        ]
+    }
+}
